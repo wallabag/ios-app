4fca0876db94165271326f6f0b66700d4f732b7b
Maxime <maxime@MacBook-Pro-de-maxime.local>
2016-10-20 13:59:17 +0200

    Login ArticlesTable view
    Poc Api

---
+++ wallabag/ArticlesTableViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ArticlesTableViewController.swift
+//  wallabag
+//
+//  Created by maxime marinel on 20/10/2016.
+//  Copyright © 2016 maxime marinel. All rights reserved.
+//
+
+import UIKit
+
+final class ArticlesTableViewController: UITableViewController {
+
+    var articles = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        WallabagApi.retrieveArticle { articles in
+            self.articles = articles
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        cell.textLabel?.text = articles[indexPath.item]
+    
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ wallabag/WallabagApi.swift
@@ -0,0 +1,65 @@
+//
+//  WallabagApi.swift
+//  wallabag
+//
+//  Created by maxime marinel on 20/10/2016.
+//  Copyright © 2016 maxime marinel. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+final class WallabagApi {
+    static fileprivate var endpoint:String?
+    static fileprivate var clientId:String?
+    static fileprivate var clientSecret:String?
+    static fileprivate var username:String?
+    static fileprivate var password:String?
+    static fileprivate var configured:Bool = false
+    
+    static fileprivate var access_token:String?
+    
+    static func configureApi(endpoint:String, clientId:String, clientSecret:String, username:String, password:String) {
+        self.endpoint = endpoint
+        self.clientId = clientId
+        self.clientSecret = clientSecret
+        self.username = username
+        self.password = password
+        
+        configured = true
+    }
+    
+    static func requestToken(_ completion: @escaping () -> ()) {
+        let parameters = ["grant_type" : "password", "client_id": clientId!, "client_secret": clientSecret!, "username" : username!, "password": password!]
+        
+        Alamofire.request(endpoint! + "/oauth/v2/token", parameters: parameters).responseJSON { response in
+            if let result = response.result.value {
+                let JSON = result as! NSDictionary
+                
+                if let token = JSON["access_token"] as? String {
+                    access_token = token
+                    
+                    completion()
+                }
+            }
+        }
+    }
+    
+    static func retrieveArticle(_ completion: @escaping ([String]) -> ()) {
+        let parameters = ["access_token" : access_token!]
+        var articles = [String]()
+        
+        Alamofire.request(endpoint! + "/api/entries.html", parameters: parameters).responseJSON { response in
+            if let result = response.result.value {
+                if let JSON = result as? NSDictionary {
+                    if let embedded = JSON["_embedded"] as? NSDictionary {
+                        for item in embedded["items"] as! [NSDictionary] {
+                            articles.append(item["title"] as! String)
+                        }
+                    }
+                }
+            }
+            completion(articles)
+        }
+    }
+}
