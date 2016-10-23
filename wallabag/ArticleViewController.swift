fe15cc5d20dc1d1e029587a4fd54583ac1f78cd8
Maxime <maxime@MacBook-Pro-de-maxime.local>
2016-10-23 22:59:49 +0200

    clean code

---
+++ wallabag/ArticleViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ArticleViewController.swift
+//  wallabag
+//
+//  Created by maxime marinel on 23/10/2016.
+//  Copyright © 2016 maxime marinel. All rights reserved.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+
+    var article:Article!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = article.title
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- wallabag/ArticlesTableViewController.swift
+++ wallabag/ArticlesTableViewController.swift
@@ -21,13 +21,8 @@ final class ArticlesTableViewController: UITableViewController {
                 self.tableView.reloadData()
             }
         }
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -40,13 +35,13 @@ final class ArticlesTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "articleIdentifier", for: indexPath)
 
         cell.textLabel?.text = articles[indexPath.item].title
     
         return cell
     }
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -82,14 +77,19 @@ final class ArticlesTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "readArticle" {
+            if let cell = sender as? UITableViewCell {
+                let indexPath = tableView.indexPath(for: cell)
+                if let index = indexPath?.row {
+                    if let controller = segue.destination as? ArticleViewController {
+                        controller.article = articles[index]
+                    }
+                }
+            }
+        }
     }
-    */
-
 }
