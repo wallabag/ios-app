97c214a11afd1718d89051b5b2c268815c3eee77
Maxime <maxime@MacBook-Pro-de-maxime.local>
2016-10-26 23:25:19 +0200

    Clean file

--- wallabag/ClientIdViewController.swift
+++ wallabag/ClientIdViewController.swift
@@ -30,5 +30,4 @@ final class ClientIdViewController: UIViewController {
             controller.clientSecret = clientSecret.text!
         }
     }
-
 }
--- wallabag/HomeViewController.swift
+++ wallabag/HomeViewController.swift
@@ -10,20 +10,4 @@ import UIKit
 
 final class HomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-
 }
