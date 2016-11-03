dd6da6c6e70715bf4e97e98ce916e3553c2a93b4
Maxime <maxime@MacBook-Pro-de-maxime.local>
2016-11-03 21:19:53 +0100

    Register label

--- wallabag/HomeViewController.swift
+++ wallabag/HomeViewController.swift
@@ -11,15 +11,20 @@ import CoreData
 
 final class HomeViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var registerButton: UIButton!
 
-        if let server = CoreData.findAll("Server").first as? Server {
+    override func viewDidLoad() {
+        if let server = CoreData.findAll("Server").last as? Server {
             WallabagApi.configureApi(endpoint: server.host, clientId: server.client_id, clientSecret: server.client_secret, username: server.username, password: server.password)
             WallabagApi.requestToken() { success in
                 if success {
                     self.performSegue(withIdentifier: "toArticles", sender: nil)
+                } else {
+                    self.registerButton.isEnabled = true
                 }
             }
+        } else {
+            registerButton.isEnabled = true
         }
     }
 }
