ff2e91cb191a7d38b736357238c1f48bfdd4afc7
Maxime <maxime@MacBook-Pro-de-maxime.local>
2016-11-04 20:29:14 +0100

    fallback 9.3

--- wallabag/CoreData.swift
+++ wallabag/CoreData.swift
@@ -13,34 +13,100 @@ final class CoreData: NSObject {
 
     static var containerName: String?
 
-    static var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: containerName!)
-        container.loadPersistentStores { storeDescription, error in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+    @available(iOS 10.0, *)
+    static var persistentContainer: NSPersistentContainer? = {
+        if #available(iOS 10.0, *) {
+            let container = NSPersistentContainer(name: containerName!)
+            container.loadPersistentStores { storeDescription, error in
+                if let error = error as NSError? {
+                    fatalError("Unresolved error \(error), \(error.userInfo)")
+                }
             }
+            return container
         }
-        return container
+        return nil
+    }()
+
+    @available(iOS 9.3, *)
+    static var managedObjectContext: NSManagedObjectContext = {
+        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+        let coordinator = persistentStoreCoordinator
+        var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+
+    @available(iOS 9.3, *)
+    static var applicationDocumentsDirectory: URL = {
+        // The directory the application uses to store the Core Data store file. This code uses a directory named "fr.district-web.Radio_Stream_Live" in the application's documents Application Support directory.
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return urls[urls.count - 1]
+    }()
+
+    @available(iOS 9.3, *)
+    static var managedObjectModel: NSManagedObjectModel = {
+        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+        let modelURL = Bundle.main.url(forResource: containerName!, withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelURL)!
+    }()
+
+    @available(iOS 9.3, *)
+    static var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+        // Create the coordinator and store
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        let url = applicationDocumentsDirectory.appendingPathComponent(containerName! + ".sqlite")
+        var failureReason = "There was an error creating or loading the application's saved data."
+        do {
+            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: nil)
+        } catch {
+            // Report any error we got.
+            var dict = [String: AnyObject]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject?
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason as AnyObject?
+
+            dict[NSUnderlyingErrorKey] = error as NSError
+            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            // Replace this with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+            abort()
+        }
+
+        return coordinator
     }()
 
     static var context: NSManagedObjectContext {
-        return persistentContainer.viewContext
+        if #available(iOS 10.0, *) {
+            return persistentContainer!.viewContext
+        } else {
+            return managedObjectContext
+        }
     }
 
     static func save() throws {
         try context.save()
     }
 
-    static func execute(_ request: NSPersistentStoreRequest) -> NSPersistentStoreResult {
-        let result = try! persistentContainer.viewContext.execute(request)
+    /*static func execute(_ request: NSPersistentStoreRequest) -> NSPersistentStoreResult? {
+     if #available(iOS 10.0, *) {
+     let result = try! persistentContainer!.viewContext.execute(request)
+     return result
+     } else {
+     // Fallback on earlier versions
+     }
 
-        return result
-    }
+     return nil
+     }*/
 
     static func fetch(_ request: NSPersistentStoreRequest) -> [NSManagedObject] {
-        let result = try! persistentContainer.viewContext.fetch(request as! NSFetchRequest<NSFetchRequestResult>)
-
-        return result as! [NSManagedObject]
+        if #available(iOS 10.0, *) {
+            let result = try! persistentContainer!.viewContext.fetch(request as! NSFetchRequest<NSFetchRequestResult>)
+            return result as! [NSManagedObject]
+        } else {
+            let result = try! managedObjectContext.fetch(request as! NSFetchRequest<NSFetchRequestResult>)
+            return result as! [NSManagedObject]
+        }
     }
 
     static func findAll(_ entity: String) -> [NSManagedObject] {
