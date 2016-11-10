7cfb3dc368a0bb398927b07e4de22d84cda3d5e5
Maxime <maxime@MacBook-Pro-de-maxime.local>
2016-11-10 13:49:52 +0100

    UIBarItem Font

--- wallabag/AppDelegate.swift
+++ wallabag/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         CoreData.containerName = "wallabag"
 
+        UIBarButtonItem.appearance().setTitleTextAttributes([
+            NSFontAttributeName: UIFont(name: "UbuntuTitling-Bold", size: 15.0)!
+        ], for: .normal)
+
         return true
     }
 
