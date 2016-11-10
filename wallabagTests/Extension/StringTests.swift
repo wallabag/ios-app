90151db185aebf24550862fa2dcacccf35d8ca72
Maxime Marinel <m.marinel@motoblouz.com>
2016-11-10 17:44:01 +0100

    WIP test string extension

--- wallabagTests/Extension/StringTests.swift
+++ wallabagTests/Extension/StringTests.swift
@@ -14,4 +14,18 @@ class StringTests: XCTestCase {
     func testDateWithWrongFormatReturnNil() {
         XCTAssertNil("".date)
     }
+
+    func testDateWithGoodFormatReturnDate() {
+    	let date = "2016-11-10T17:34:20+0100".date
+
+    	let components = Calendar.current.dateComponents([.hour, .minut, .second, .day, .month, .year], from: date)
+
+        XCTAssertNotNil(date)
+        XCTAssertEqual(2016, components.year)
+        XCTAssertEqual(11, components.month)
+        XCTAssertEqual(10, components.day)
+        XCTAssertEqual(17, components.hour)
+        XCTAssertEqual(34, components.minut)
+        XCTAssertEqual(20, components.second)
+    }
 }
