11115d8eadc63c5c6ab4df7f85ef720d0c9de136
Maxime <maxime@MacBook-Pro-de-maxime.local>
2016-11-19 13:15:49 +0100

    Registration add check fix #21

--- wallabag/Controller/ServerViewController.swift
+++ wallabag/Controller/ServerViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import Alamofire
 
 final class ServerViewController: UIViewController {
 
     @IBOutlet weak var server: UITextField!
 
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if validateServer(string: server.text!) {
+            return true
+        }
+
+        let alertController = UIAlertController(title: "Error", message: "Whoops looks like something went wrong. Check the url, don't forget http or https", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        present(alertController, animated: true, completion: nil)
+
+        return false
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let controller = segue.destination as? ClientIdViewController {
             controller.server = server.text!
         }
+    }
+
+    func validateServer(string: String) -> Bool {
+        guard let url = URL(string: string) else { return false }
+        if !UIApplication.shared.canOpenURL(url) { return false }
+
+        let regEx = "((https|http)://)((\\w|-)+)(([.]|[/])((\\w|-)+))+"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", argumentArray: [regEx])
+        return predicate.evaluate(with: string)
     }
 }
