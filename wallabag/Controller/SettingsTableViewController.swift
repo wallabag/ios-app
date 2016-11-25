2ae7bb97d906ee88bbfe9be9821ce762a252639d
Maxime <maxime@MacBook-Pro-de-maxime.local>
2016-11-25 20:57:18 +0100

    Feedback from internal testing fix #35

---
+++ wallabag/Controller/SettingsTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ParameterTableViewController.swift
+//  wallabag
+//
+//  Created by maxime marinel on 23/11/2016.
+//  Copyright © 2016 maxime marinel. All rights reserved.
+//
+
+import UIKit
+
+final class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        prepareDefaultList()
+
+        tableView.separatorStyle = .none
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            for row in 0 ... tableView.numberOfRows(inSection: indexPath.section) {
+                tableView.cellForRow(at: IndexPath(row: row, section: indexPath.section))?.accessoryType = .none
+            }
+
+            if let cell = tableView.cellForRow(at: indexPath) {
+                Setting.setDefaultMode(mode: RetrieveMode(rawValue: cell.reuseIdentifier!)!)
+                cell.accessoryType = .checkmark
+                cell.selectionStyle = .none
+            }
+        }
+    }
+
+    fileprivate func prepareDefaultList() {
+        for row in 0 ... tableView.numberOfRows(inSection: 0) {
+            if let cell = tableView.cellForRow(at: IndexPath(row: row, section: 0)) {
+                if RetrieveMode(rawValue: cell.reuseIdentifier!)! == Setting.getDefaultMode() {
+                    cell.accessoryType = .checkmark
+                }
+            }
+        }
+    }
+}
