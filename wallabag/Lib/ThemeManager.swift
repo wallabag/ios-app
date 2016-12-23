88de5c0b83a6eb422b20a4cdd296d75305622d9e
Jeremy Benoist <jeremy.benoist@gmail.com>
2016-12-23 07:36:47 +0100

    Update light theme

--- wallabag/Lib/ThemeManager.swift
+++ wallabag/Lib/ThemeManager.swift
@@ -12,33 +12,44 @@ struct ThemeManager {
 
     // Add theme here and dont forget add in allThemes
     enum Theme: String {
-        case light
+        case white
         case night
+        case light
 
-        static let allThemes: [Theme] = [light, night]
+        static let allThemes: [Theme] = [white, light, night]
 
+        // color for the menu and listing articles
         var color: UIColor {
             switch self {
             case .night:
-                return UIColor.black
+                return UIColor.init(red: 153, green: 153, blue: 153, alpha: 1)
+            case .light:
+                //return UIColor.init(red: 64, green: 64, blue: 64, alpha: 1)
+                return UIColor.green
             default:
                 return UIColor.black
             }
         }
 
+        // color for navigation button (back, delete, star, share)
         var tintColor: UIColor {
             switch self {
             case .night:
-                return UIColor.black
+                return UIColor.init(red: 153, green: 153, blue: 153, alpha: 1)
+            case .light:
+                return UIColor.init(red: 64, green: 64, blue: 64, alpha: 1)
             default:
                 return UIColor.black
             }
         }
 
+        // background of the article (outside the webview) & the listing
         var backgroundColor: UIColor {
             switch self {
             case .night:
-                return UIColor.gray
+                return UIColor.init(red: 17, green: 17, blue: 17, alpha: 1)
+            case .light:
+                return UIColor.init(red: 246, green: 239, blue: 220, alpha: 1)
             default:
                 return UIColor.white
             }
@@ -48,6 +59,9 @@ struct ThemeManager {
             switch self {
             case .night:
                 let image = #imageLiteral(resourceName: "navBackgroundNight")
+                return image
+            case .light:
+                let image = #imageLiteral(resourceName: "navBackgroundSoft")
                 return image
             default:
                 return nil
