1e2352fabab3fe22bf1b4a1a79cfba976ce39b91
Maxime <maxime.m62@gmail.com>
2017-02-23 09:07:01 +0100

    Remove post view

--- bagit/ShareViewController.swift
+++ bagit/ShareViewController.swift
@@ -9,43 +9,38 @@
 import UIKit
 import Social
 
-class ShareViewController: SLComposeServiceViewController {
+@objc(ShareViewController)
+class ShareViewController: UIViewController {
 
-    let user = UserDefaults(suiteName: "group.wallabag.share_extension")
+    override func viewWillAppear(_ animated: Bool) {
+        DispatchQueue.main.async { [weak self] in
+            guard let user = UserDefaults(suiteName: "group.wallabag.share_extension"),
+                let item = self?.extensionContext?.inputItems.first as? NSExtensionItem,
+                let itemProvider = item.attachments?.first as? NSItemProvider else {
+                    self?.extensionContext?.cancelRequest(withError: NSError())
+                    return
+            }
 
-    override func isContentValid() -> Bool {
-        return user?.bool(forKey: "serverConfigured") ?? false
-    }
-
-    override func didSelectPost() {
-        if let item = extensionContext?.inputItems.first as? NSExtensionItem {
-            if let itemProvider = item.attachments?.first as? NSItemProvider {
-                if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
-                    itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, _) -> Void in
-                        if let shareURL = url as? NSURL {
-                            guard let host = self.user?.value(forKey: "host") as? String,
-                                let clientId = self.user?.value(forKey: "clientId") as? String,
-                                let clientSecret = self.user?.value(forKey: "clientSecret") as? String,
-                                let username = self.user?.value(forKey: "username") as? String,
-                                let password = self.user?.value(forKey: "password") as? String
-                                else {
-                                    return
-                            }
-                            let server = Server(host: host, client_secret: clientSecret, client_id: clientId, username: username, password: password)
-                            WallabagApi.configureApi(from: server)
-                            WallabagApi.requestToken { _ in
-                                WallabagApi.addArticle(shareURL as URL, completion: { _ in
-                                    self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
-                                })
-                            }
-                        }
-                    })
-                }
+            if  itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+                itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, _) -> Void in
+                    guard let shareURL = url as? NSURL,
+                        let host = user.value(forKey: "host") as? String,
+                        let clientId = user.value(forKey: "clientId") as? String,
+                        let clientSecret = user.value(forKey: "clientSecret") as? String,
+                        let username = user.value(forKey: "username") as? String,
+                        let password = user.value(forKey: "password") as? String
+                        else {
+                            return
+                    }
+                    let server = Server(host: host, client_secret: clientSecret, client_id: clientId, username: username, password: password)
+                    WallabagApi.configureApi(from: server)
+                    WallabagApi.requestToken { _ in
+                        WallabagApi.addArticle(shareURL as URL, completion: { _ in
+                            self?.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+                        })
+                    }
+                })
             }
         }
-    }
-
-    override func configurationItems() -> [Any]! {
-        return []
     }
 }
