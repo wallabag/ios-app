e504871c045ea65f50f2f2326bfcd7f7b1e6eb4f
Maxime Marinel <m.marinel@motoblouz.com>
2017-04-06 15:12:22 +0200

    Change fading time for wallabag extension fix #83

--- bagit/ShareViewController.swift
+++ bagit/ShareViewController.swift
@@ -32,18 +32,18 @@ class ShareViewController: UIViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        DispatchQueue.main.async { [weak self] in
+        DispatchQueue.main.async { [unowned self] in
             guard let user = UserDefaults(suiteName: "group.wallabag.share_extension"),
                 let host = user.value(forKey: "host") as? String,
                 let clientId = user.value(forKey: "clientId") as? String,
                 let clientSecret = user.value(forKey: "clientSecret") as? String,
                 let username = user.value(forKey: "username") as? String,
                 let password = user.value(forKey: "password") as? String else {
-                    self?.extensionContext?.cancelRequest(withError: NSError())
+                    self.extensionContext?.cancelRequest(withError: NSError())
                     return
             }
 
-            for item in (self?.extensionContext?.inputItems as? [NSExtensionItem])! {
+            for item in (self.extensionContext?.inputItems as? [NSExtensionItem])! {
                 for attachements in (item.attachments as? [NSItemProvider])! {
                     if attachements.hasItemConformingToTypeIdentifier("public.url") {
                         attachements.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, _) -> Void in
@@ -52,15 +52,15 @@ class ShareViewController: UIViewController {
                                 WallabagApi.configureApi(from: server)
                                 WallabagApi.requestToken { _ in
                                     WallabagApi.addArticle(shareURL as URL, completion: { _ in
-                                        UIView.animate(withDuration: 2.0, animations: {
-                                            self?.notificationView.alpha = 0.0
+                                        UIView.animate(withDuration: 0.5, animations: {
+                                            self.notificationView.alpha = 0.0
                                         }, completion: { _ in
-                                            self?.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+                                            self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
                                         })
                                     })
                                 }
                             } else {
-                                self?.extensionContext?.cancelRequest(withError: NSError())
+                                self.extensionContext?.cancelRequest(withError: NSError())
                             }
                         })
                     }
