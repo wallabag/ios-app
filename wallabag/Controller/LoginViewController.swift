2e294a2476c00a2ff4d86bdd7085e28ec4e2b898
Maxime <maxime.m62@gmail.com>
2017-05-07 20:52:20 +0200

    Clean LoginViewController

--- wallabag/Controller/LoginViewController.swift
+++ wallabag/Controller/LoginViewController.swift
@@ -18,16 +18,10 @@ final class LoginViewController: UIViewController {
     @IBOutlet weak var password: UITextField!
 
     @IBAction func login(_ sender: UIButton) {
-        //let server = Server(host: self.server,
-        //                    client_secret: self.clientSecret,
-        //                    client_id: self.clientId)
-
-        //WallabagApi.configureApi(from: server, userStorage: UserDefaults(suiteName: "group.wallabag.share_extension")!)
         sender.isEnabled = false
 
         WallabagApi.requestToken(username: username.text!, password: password.text!) { success, error in
             if success {
-                //Setting.set(server: server)
                 self.performSegue(withIdentifier: "toArticles", sender: nil)
             } else {
                 let alert = UIAlertController(title: "Error", message: error, preferredStyle: .alert)
