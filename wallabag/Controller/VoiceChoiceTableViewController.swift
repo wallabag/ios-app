6b46b5f3945c8e799ee3ee48b7e326e0b5bcb1f2
Maxime <maxime.m62@gmail.com>
2017-07-30 22:01:09 +0200

    Clean code

--- wallabag/Controller/VoiceChoiceTableViewController.swift
+++ wallabag/Controller/VoiceChoiceTableViewController.swift
@@ -12,10 +12,6 @@ import AVFoundation
 final class VoiceChoiceTableViewController: UITableViewController {
 
     let voices: [AVSpeechSynthesisVoice] = AVSpeechSynthesisVoice.speechVoices()
-    // MARK: - Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return voices.count
