86cc001c21d56b9c01d8df4621826a168d0114b3
Marinel maxime <maxime.m62@gmail.com>
2018-12-21 09:23:51 +0100

    Add 2.3.4 server version

--- wallabagKit/WallabagVersion.swift
+++ wallabagKit/WallabagVersion.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public struct WallabagVersion {
     public enum SupportedVersion: String {
+        case v2dot3dot4 = "\"2.3.4\""
         case v2dot3dot3 = "\"2.3.3\""
         case v2dot3dot2 = "\"2.3.2\""
         case unsupported
