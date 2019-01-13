cc8832f34f96f1b714a79ad8f727746296ffdfde
Marinel maxime <maxime.m62@gmail.com>
2019-01-13 13:38:15 +0100

    Enable version 2.3.6

--- wallabagKit/WallabagVersion.swift
+++ wallabagKit/WallabagVersion.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public struct WallabagVersion {
     public enum SupportedVersion: String {
+        case v2dot3dot6 = "\"2.3.6\""
         case v2dot3dot5 = "\"2.3.5\""
         case v2dot3dot4 = "\"2.3.4\""
         case v2dot3dot3 = "\"2.3.3\""
