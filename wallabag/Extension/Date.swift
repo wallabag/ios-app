d1c1547998993336865db728caddccfb85c363dc
Maxime Marinel <bourvill@users.noreply.github.com>
2019-04-08 13:54:06 +0200

    Feature/test (#169)
    * Remove xcodegen
    
    * Travis
    
    * Update travis
    
    * Travis hell
    
    * Fix test
    
    * Workaround xcode10 and travis
    
    * Setup travis
    
    * Upgrade xcode....
    
    * Add test

--- wallabag/Extension/Date.swift
+++ wallabag/Extension/Date.swift
@@ -11,10 +11,7 @@ import Foundation
 extension Date {
     static func fromISOString(_ string: String) -> Date? {
         let dateFormatter = DateFormatter()
-        _ = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        _ = "2018-04-09T10:27:23+0200"
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssX"
-
         return dateFormatter.date(from: string)
     }
 }
---
+++ wallabagTests/Extension/DateTests.swift
@@ -0,0 +1,18 @@
+//
+//  DateTests.swift
+//  wallabagTests
+//
+//  Created by maxime marinel on 08/04/2019.
+//
+
+@testable import wallabag
+import XCTest
+
+class DateTests: XCTestCase {
+
+    func testDateFromIsoString() {
+        let date = Date.fromISOString("1977-04-22T01:00:00-05:00")
+        #warning("Add better test")
+        XCTAssertTrue(nil != date)
+    }
+}
