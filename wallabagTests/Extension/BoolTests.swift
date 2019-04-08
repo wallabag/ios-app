7adba06979d4056e31c7996d4b53718fa78d2210
Marinel maxime <maxime.m62@gmail.com>
2019-04-08 13:52:53 +0200

    Add test

---
+++ wallabagTests/Extension/BoolTests.swift
@@ -0,0 +1,16 @@
+//
+//  BoolTests.swift
+//  wallabagTests
+//
+//  Created by maxime marinel on 08/04/2019.
+//
+@testable import wallabag
+import XCTest
+
+class BoolTests: XCTestCase {
+
+    func testInt() {
+        XCTAssertEqual(1, true.int)
+        XCTAssertEqual(0, false.int)
+    }
+}
