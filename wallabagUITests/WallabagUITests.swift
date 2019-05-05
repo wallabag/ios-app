7da4401b2de7934f0795c5a2335c5c63c5a6854f
Marinel maxime <maxime.m62@gmail.com>
2019-05-05 11:56:38 +0200

    Update for lint

--- wallabagUITests/WallabagUITests.swift
+++ wallabagUITests/WallabagUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class wallabagUITests: XCTestCase {
+class WallabagUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
