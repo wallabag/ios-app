18c9aae2d1a81b76f9876bb1e1a08547a39d1ff9
Marinel maxime <maxime.m62@gmail.com>
2019-07-11 22:56:14 +0200

    Show must go on

---
+++ wallabag/DBData.swift
@@ -0,0 +1,51 @@
+//
+//  DBData.swift
+//  wallabag
+//
+//  Created by Marinel Maxime on 11/07/2019.
+//
+
+import SwiftUI
+import Combine
+import RealmSwift
+
+final class DBData: BindableObject  {
+    
+    let didChange = PassthroughSubject<DBData, Never>()
+    
+    private var notificationTokens: [NotificationToken] = []
+    var results: Results<Entry>?
+    
+    init() {
+        let realm = try! Realm()
+        
+        results = realm.objects(Entry.self)
+        // Observe changes in the underlying model
+        self.notificationTokens.append(results!.observe { _ in
+            self.didChange.send(self)
+        })
+    }
+}
+
+class BindableResults<Element>: BindableObject where Element: RealmSwift.RealmCollectionValue {
+    
+    let didChange = PassthroughSubject<Void, Never>()
+    
+    let results: Results<Element>
+    private var token: NotificationToken!
+    
+    init(results: Results<Element>) {
+        self.results = results
+        lateInit()
+    }
+    
+    func lateInit() {
+        token = results.observe { _ in
+            self.didChange.send(())
+        }
+    }
+    
+    deinit {
+        token.invalidate()
+    }
+}
---
+++ wallabag/View/ArticleListView.swift
@@ -0,0 +1,28 @@
+//
+//  ArticleListView.swift
+//  wallabag
+//
+//  Created by Marinel Maxime on 11/07/2019.
+//
+
+import SwiftUI
+import RealmSwift
+
+struct ArticleListView : View {
+    @ObjectBinding var entries = BindableResults<Entry>(results: try! Realm().objects(Entry.self))
+
+    var body: some View {
+        List(entries.results.identified(by: \.id)) { entry in
+            Text("test")
+        }
+        
+    }
+}
+
+#if DEBUG
+struct ArticleListView_Previews : PreviewProvider {
+    static var previews: some View {
+        ArticleListView()
+    }
+}
+#endif
