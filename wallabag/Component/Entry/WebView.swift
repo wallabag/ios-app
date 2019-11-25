e62eb56efa65cac42661274ce783d89fe08354a8
Marinel maxime <maxime.m62@gmail.com>
2019-11-25 13:48:11 +0100

    Create coordinator for WebView Fix tap on link in article -> Now open Safari

--- wallabag/Component/Entry/WebView.swift
+++ wallabag/Component/Entry/WebView.swift
@@ -5,14 +5,52 @@
 //  Created by Marinel Maxime on 18/07/2019.
 //
 
+import SafariServices
 import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
     var entry: Entry
 
-    func makeUIView(context _: Context) -> WKWebView {
-        WKWebView(frame: .zero)
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var webView: WebView
+
+        init(_ webView: WebView) {
+            self.webView = webView
+        }
+
+        func webView(_ webView: WKWebView, didFinish _: WKNavigation!) {
+            webView.scrollView.setContentOffset(CGPoint(x: 0.0, y: Double(self.webView.entry.screenPosition)), animated: true)
+        }
+
+        func webView(_: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+            guard let urlTarget = navigationAction.request.url else {
+                decisionHandler(.cancel)
+                return
+            }
+            let urlAbsolute = urlTarget.absoluteString
+
+            if urlAbsolute.hasPrefix(Bundle.main.bundleURL.absoluteString) || urlAbsolute == "about:blank" {
+                decisionHandler(.allow)
+                return
+            }
+
+            let safariController = SFSafariViewController(url: urlTarget)
+            safariController.modalPresentationStyle = .overFullScreen
+
+            UIApplication.shared.open(urlTarget, options: [:], completionHandler: nil)
+            decisionHandler(.cancel)
+        }
+    }
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webview = WKWebView(frame: .zero)
+        webview.navigationDelegate = context.coordinator
+        return webview
     }
 
     func updateUIView(_ view: WKWebView, context _: Context) {
