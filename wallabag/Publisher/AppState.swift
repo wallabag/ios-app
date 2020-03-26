67fab3e4d82306c456d0f74b6b845cb39ff9bce5
Marinel maxime <maxime.m62@gmail.com>
2020-03-26 09:21:29 +0100

    Arrange logout

--- wallabag/Publisher/AppState.swift
+++ wallabag/Publisher/AppState.swift
@@ -36,12 +36,6 @@ class AppState: NSObject, ObservableObject {
     func logout() {
         registred = false
         session.state = .unknown
-        WallabagUserDefaults.host = ""
-        WallabagUserDefaults.accessToken = ""
-        WallabagUserDefaults.clientId = ""
-        WallabagUserDefaults.clientSecret = ""
-        WallabagUserDefaults.password = ""
-        WallabagUserDefaults.login = ""
         router.route = .registration
     }
 }
