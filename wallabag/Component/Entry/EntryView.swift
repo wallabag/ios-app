2d242bfb9a0e1c7d0afa583abd8a1d38468d5a98
Marinel maxime <maxime.m62@gmail.com>
2020-05-26 17:35:34 +0200

    Rewrite EntryView

--- wallabag/Component/Entry/EntryView.swift
+++ wallabag/Component/Entry/EntryView.swift
@@ -28,30 +28,37 @@ struct EntryView: View {
                 Spacer()
             }.padding()
             WebView(entry: entry)
-            HStack {
-                DeleteEntryButton(entry: entry, showText: false) {
-                    self.router.load(.entries)
-                }.hapticNotification(.warning)
-                Spacer()
-                Button(action: {
-                    self.openInSafari(self.entry.url)
-                }, label: {
-                    Image(systemName: "safari")
-                })
-                Button(action: {
-                    self.showTag.toggle()
-                }, label: {
-                    Image(systemName: self.showTag ? "tag.fill" : "tag")
-                }).buttonStyle(PlainButtonStyle())
-                StarEntryButton(entry: entry, showText: false).hapticNotification(.success)
-                ArchiveEntryButton(entry: entry, showText: false).hapticNotification(.success)
-            }.font(.system(size: 20))
-                .padding()
+            bottomBar
             // PlayerView()
         }.sheet(isPresented: $showTag) {
             TagListFor(tagsForEntry: TagsForEntryPublisher(entry: self.entry))
                 .environment(\.managedObjectContext, CoreData.shared.viewContext)
         }
+    }
+
+    private var bottomBar: some View {
+        HStack {
+            DeleteEntryButton(entry: entry, showText: false) {
+                self.router.load(.entries)
+            }.hapticNotification(.warning)
+            Spacer()
+            Button(action: {
+                self.openInSafari(self.entry.url)
+            }, label: {
+                Image(systemName: "safari")
+                }).buttonStyle(PlainButtonStyle())
+            Spacer()
+            Button(action: {
+                self.showTag.toggle()
+            }, label: {
+                Image(systemName: self.showTag ? "tag.fill" : "tag")
+            }).buttonStyle(PlainButtonStyle())
+            Spacer()
+            StarEntryButton(entry: entry, showText: false).hapticNotification(.success)
+            Spacer()
+            ArchiveEntryButton(entry: entry, showText: false).hapticNotification(.success)
+        }.font(.system(size: 20))
+            .padding()
     }
 }
 
