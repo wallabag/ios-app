f6c13dcdbcec4448fa46ee83a5b0f5d41ff369a3
Marinel maxime <maxime.m62@gmail.com>
2020-11-29 21:51:39 +0100

    Add metrickit

--- wallabag/AppDelegate.swift
+++ wallabag/AppDelegate.swift
@@ -8,15 +8,21 @@
 
 import CoreSpotlight
 import Logging
+import MetricKit
 import Swinject
 import UIKit
 import UserNotifications
 import WallabagKit
 
+extension AppDelegate: MXMetricManagerSubscriber {
+    func didReceive(_: [MXMetricPayload]) {}
+}
+
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        MXMetricManager.shared.add(self)
         #if DEBUG
             let args = ProcessInfo.processInfo.arguments
 
@@ -32,6 +38,10 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidFinishLaunching(_: UIApplication) {
         UIApplication.shared.beginReceivingRemoteControlEvents()
+    }
+
+    func applicationWillTerminate(_: UIApplication) {
+        MXMetricManager.shared.remove(self)
     }
 
     private func requestBadge() {
