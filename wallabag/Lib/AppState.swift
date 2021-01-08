de24b313c05b70f60b326b2e59668af268a48231
Marinel maxime <maxime.m62@gmail.com>
2021-01-08 22:54:17 +0100

    Arrange menu fix #327

--- wallabag/Lib/AppState.swift
+++ wallabag/Lib/AppState.swift
@@ -6,7 +6,6 @@ class AppState: NSObject, ObservableObject {
 
     @Published var registred: Bool = false {
         didSet {
-            logger.info("App state registred \(self.registred)")
             WallabagUserDefaults.registred = registred
         }
     }
