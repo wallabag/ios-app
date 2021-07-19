96330c5afc730f18cc184f71d52c1e363e9f03d0
Marinel maxime <maxime.m62@gmail.com>
2021-07-19 16:38:50 +0200

    Remove old Router PresentedView

--- iOS/Features/Router/PresentedView.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import SwiftUI
-
-@available(*, deprecated, message: "Remove this old view presenter")
-struct PresentedView: Identifiable {
-    let id: String
-    let wrappedView: AnyView
-}
