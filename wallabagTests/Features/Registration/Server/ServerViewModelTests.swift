2cdc0495ba8d8021e98685e44ef01531bcdf9992
Liquidsoul <liquidsoul@liquidsoul.fr>
2024-12-01 22:44:51 +0100

    test: fix ServerViewModelTests following a previous update
    - `.isValid` property is not a @Published anymore
    - changing the `.url` property does not apply it to `UserDefaults` instantly
    - add a test to check the impact of `goNext()` to `UserDefaults`
    - replace `class func setUp()` with `func setUp()` because the reset of
      `WallabagUserDefaults.host` must be done for each test. the `class` one is
      only run once for the whole `XCTestCase` whereas the other one is run before
      each test.

--- wallabagTests/Features/Registration/Server/ServerViewModelTests.swift
+++ wallabagTests/Features/Registration/Server/ServerViewModelTests.swift
@@ -6,7 +6,7 @@ import XCTest
 final class ServerViewModelTests: XCTestCase {
     var cancellable = Set<AnyCancellable>()
 
-    override class func setUp() {
+    override func setUp() {
         WallabagUserDefaults.host = ""
     }
 
@@ -19,45 +19,37 @@ final class ServerViewModelTests: XCTestCase {
     func testInvalidUrl() {
         let model = ServerViewModel()
 
-        let expectation = XCTestExpectation(description: "Invalid url")
-
         XCTAssertEqual("", model.url)
         XCTAssertFalse(model.isValid)
 
-        model.$isValid
-            .dropFirst()
-            .sink(receiveValue: {
-                XCTAssertFalse($0)
-                expectation.fulfill()
-            })
-            .store(in: &cancellable)
-
         model.url = "azerty"
         XCTAssertEqual("", WallabagUserDefaults.host)
-
-        wait(for: [expectation], timeout: 1)
+        XCTAssertFalse(model.isValid)
     }
 
     func testValidUrl() {
         let model = ServerViewModel()
 
-        let expectation = XCTestExpectation(description: "Valid url")
-
         XCTAssertEqual("", model.url)
         XCTAssertEqual("", WallabagUserDefaults.host)
         XCTAssertFalse(model.isValid)
 
-        model.$isValid
-            .dropFirst()
-            .sink(receiveValue: {
-                XCTAssertTrue($0)
-                expectation.fulfill()
-            })
-            .store(in: &cancellable)
         model.url = "https://wallabag.it"
 
-        XCTAssertEqual("https://wallabag.it", WallabagUserDefaults.host)
+        XCTAssertEqual("", WallabagUserDefaults.host)
+        XCTAssertTrue(model.isValid)
+    }
 
-        wait(for: [expectation], timeout: 1)
+    func testUpdateHostOnGoNext() {
+        let model = ServerViewModel()
+
+        XCTAssertEqual("", model.url)
+        XCTAssertEqual("", WallabagUserDefaults.host)
+        XCTAssertFalse(model.isValid)
+        model.url = "https://wallabag.it"
+        XCTAssertEqual("", WallabagUserDefaults.host)
+
+        model.goNext()
+        XCTAssertEqual("https://wallabag.it", WallabagUserDefaults.host)
     }
 }
